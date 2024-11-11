9723d11056323dbceb8e21e97407741858a41c0e
Dany Meyer <dany.meyer@hnu.de>
2024-11-11 12:14:25 +0100

    erster Test

--- Testclass.java
+++ Testclass.java
@@ -1,9 +1,8 @@
 public class Testclass{
 
-  public static void main (String[] args){
+    // Kommentar
+    public static void main (String[] args){
       System.out.println("Hello, there!");
-      
-      // das ist ein Kommentar
-  }
+    }
 
 }
