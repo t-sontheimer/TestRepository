3c1e4e3340bd8c64f7ac9a6b487e15e608cc1709
t-sontheimer <tobias.sontheimer@student.hnu.de>
2024-11-13 10:38:16 +0100

    Update Testclass.java

--- Testclass.java
+++ Testclass.java
@@ -2,7 +2,7 @@ public class Testclass{
 
     // Kommentar
     public static void main (String[] args){
-        System.out.println("Hello zusammen");
+        System.out.println("1Hell zusammen");
     }
 
 }
